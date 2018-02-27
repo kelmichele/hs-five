afb9de51764c66b64269689b53694df38456820e
Kelli Parris <kelli@hsdesignhouse.com>
2018-02-27 11:47:42 -0600

    shopify page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'squad_goals' => 'static_pages#team', as: 'squad_goals'
   get 'contact' => 'static_pages#contact'
   get 'photo-gallery' => 'static_pages#photo-gallery'
+  get 'shopify' => 'static_pages#shopify'
 
   get '/fb', to: 'links#fb'
   get '/insta', to: 'links#insta'
